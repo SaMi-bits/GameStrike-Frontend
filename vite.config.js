ee65cebb8b0730e4e31d7e0fc1da489e677692bb
SaMi-bits <jimenezjeanpierre94@gmail.com>
2025-12-06 00:15:17 -0500

    fix: add react plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/GameStrike-Frontend/',   // 👈 SUPER IMPORTANTE
+  plugins: [react()],
+  base: '/GameStrike-Frontend/',
 })
